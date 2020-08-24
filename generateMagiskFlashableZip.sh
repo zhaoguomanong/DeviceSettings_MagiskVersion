85bd53e5d157b3b77cfd54933ad44f742bd95449
zhaoguomanong <zhaoguomanong@gmail.com>
2020-08-24 23:44:19 +0800

    20200824.4
    Signed-off-by: zhaoguomanong <zhaoguomanong@gmail.com>

--- generateMagiskFlashableZip.sh
+++ generateMagiskFlashableZip.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+cd `dirname $0`
+PROJECTROOT=`pwd`
+moduleRoot=${PROJECTROOT}/magisk_module
+
+apkFile=${PROJECTROOT}/build/outputs/apk/release/DeviceSettings_Magisk.apk
+moduleApkPath=${moduleRoot}/system/priv-app/DeviceSettings_MagiskVersion/
+
+if [[ ! -f "$apkFile" ]];then
+    echo "$apkFile not exist, fatal error"
+    exit 1
+fi
+
+if [[ ! -d "$moduleApkPath" ]];then
+    mkdir -p "$moduleApkPath"
+fi
+
+cp "$apkFile" ${moduleRoot}/system/priv-app/DeviceSettings_MagiskVersion/
+
+source ${moduleRoot}/module.prop
+zipFileName="magisk-module-DeviceSettings_MagiskVersion-${version}.zip"
+[[ -f "$zipFileName" ]] && rm -f "$zipFileName"
+
+cd ${moduleRoot}
+zip -r ${PROJECTROOT}/${zipFileName} *
+cd ${PROJECTROOT}

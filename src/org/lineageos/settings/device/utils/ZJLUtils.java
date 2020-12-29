a3b980888bf790842e9eee24bb95430826f1ca0d
zhaoguomanong <zhaoguomanong@gmail.com>
2020-12-29 18:02:37 +0800

    optimize
    Signed-off-by: zhaoguomanong <zhaoguomanong@gmail.com>

--- src/org/lineageos/settings/device/utils/ZJLUtils.java
+++ src/org/lineageos/settings/device/utils/ZJLUtils.java
@@ -11,12 +11,16 @@ import java.io.File;
 public class ZJLUtils {
 
     private static final String TAG = "ZJLUtils";
-
-    private static final String ZJL_BIN_PATH = "/system/xbin/ZJL2.0_magisk/";
-    private static final String ENABLE_ZJL = ZJL_BIN_PATH + "开启.sh";
-    private static final String DISABLE_ZJL = ZJL_BIN_PATH + "关闭.sh";
-    private static final String STATUS_ZJL = ZJL_BIN_PATH + "检测.sh";
     private static final String STATUS_DISABLED = "啥都没开";
+    private static final String ON = "开启.sh";
+    private static final String OFF = "关闭.sh";
+    private static final String STATUS = "检测.sh";
+
+
+    private static String ZJL_BIN_PATH = null;
+    private static String ENABLE_ZJL = null;
+    private static String DISABLE_ZJL = null;
+    private static String STATUS_ZJL = null;
 
 
     public static final boolean ZJL_SUPPORTED = isZJLSupported();
@@ -74,29 +78,21 @@ public class ZJLUtils {
         ThreadPoolUtil.post(new Runnable() {
             @Override
             public void run() {
-                try {
-                    if (null == listener) {
-                        return;
-                    }
-                    Log.d(TAG, "execRootCmdWithResults+++");
-
-                    final String status = RootCmd.execRootCmdWithResults("sh " + STATUS_ZJL + "\n");
-                    Log.d(TAG, "execRootCmdWithResults---");
-                    Log.d(TAG, "queryZJLStatus: status = " + status);
-                    MainApplication.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (TextUtils.isEmpty(status)) {
-                                listener.onResult(status, false);
-                                return;
-                            }
-                            listener.onResult(status, !status.contains(STATUS_DISABLED));
-                        }
-                    });
-                } catch (Exception E) {
-
+                if (null == listener) {
+                    return;
                 }
-
+                final String status = RootCmd.execRootCmdWithResults("sh -c " + STATUS_ZJL + "\n");
+                Log.d(TAG, "queryZJLStatus: status = " + status);
+                MainApplication.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (TextUtils.isEmpty(status)) {
+                            listener.onResult(status, false);
+                            return;
+                        }
+                        listener.onResult(status, !status.contains(STATUS_DISABLED));
+                    }
+                });
             }
         });
 
@@ -115,10 +111,21 @@ public class ZJLUtils {
     }
 
     private static boolean isZJLSupported() {
-        boolean isSupported = new File(ENABLE_ZJL).exists()
+        String ZJL_ROOT_PATH = RootCmd.execRootCmdWithResults(
+                "find /system/xbin/ -name "
+                        + ON + "  | awk -F '/[^/]*$' '{print $1}' | head -1");
+        if (TextUtils.isEmpty(ZJL_ROOT_PATH)
+                || !new File(ZJL_ROOT_PATH).exists()) {
+            return false;
+        }
+        ZJL_BIN_PATH = ZJL_ROOT_PATH;
+        ENABLE_ZJL = ZJL_BIN_PATH + "/" + ON;
+        DISABLE_ZJL = ZJL_BIN_PATH + "/" + OFF;
+        STATUS_ZJL = ZJL_BIN_PATH + "/" + STATUS;
+        Log.d(TAG, "ZJL_BIN_PATH = " + ZJL_BIN_PATH);
+        return new File(ENABLE_ZJL).exists()
                 && new File(DISABLE_ZJL).exists()
                 && new File(STATUS_ZJL).exists();
-        return isSupported;
     }
 
 }
